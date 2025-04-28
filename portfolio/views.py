5b3237a345e68ba10291bc4829493a66e26fff52
Martin <mvrt.new@gmail.com>
2025-04-28 18:26:37 +0300

    v2
    Научились подключать HTML-шаблоны, использовали базовый шаблон (layout), сделали главную страницу портфолио

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Привет! Это мой сайт-портфолио.")
+    context = {
+        'name': 'Ваше_имя',
+        'job': 'Ваша_профессия'
+    }
+    return render(request, 'portfolio/index.html', context)
+
+
+def contacts(request):
+    context = {
+        'phone': 'номер_телефона',
+        'telegram': 'аккаунт_в_тг'
+    }
+    return render(request, 'portfolio/contacts.html', context)
