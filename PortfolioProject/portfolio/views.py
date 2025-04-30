91be56c43af3f2353803e542d4b96c62c3f5d00e
Martin <mvrt.new@gmail.com>
2025-04-30 15:25:04 +0300

    v4

--- PortfolioProject/portfolio/views.py
+++ PortfolioProject/portfolio/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Project
 
 
 def home(request):
+    projects = Project.objects.all()  # Получаем все проекты из базы
     context = {
+        'projects': projects,
         'name': 'Ваше_имя',
         'job': 'Ваша_профессия'
     }
